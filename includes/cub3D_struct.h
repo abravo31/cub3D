bc9c22247819176056b5c1e903d8b26457e2d12a
Daniel Nieto <dani2600nieto@gmail.com>
2023-05-23 14:41:15 +0200

    First try to improve dda for handling doors

--- includes/cub3D_struct.h
+++ includes/cub3D_struct.h
@@ -294,6 +294,7 @@ int		lauch_raycasting(t_cub3D *data);
 /*Raycasting*/
 t_vec2D	ft_get_perpendicular_vec(t_vec2D dir_vec);
 void    raycasting(t_cub3D *data);
+void	_raycasting(t_cub3D *data);
 /*DDA*/
 // void	wall_finder(t_cub3D *data, t_ray *ray, t_rc *rc);
 void	wall_finder(t_cub3D *data, t_ray *ray, t_rc *rc, int i);
